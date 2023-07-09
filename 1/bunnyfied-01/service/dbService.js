87c371aaf6578903abd9456b80ae94b1b73a28f4
DONTKILLME <ashishnegi0511@gmail.com>
2023-07-09 16:49:11 +0530

    just formatted comments

--- 1/bunnyfied-01/service/dbService.js
+++ 1/bunnyfied-01/service/dbService.js
@@ -16,23 +16,18 @@ export async function insert_interns({ Name, Email, Phone }) {
       status: true,
     });
   } catch (e) {
-    console.log(`Name: ${Name} Already in DB`, e);
-    // console.log(`Email: ${Email} Already in DB`);
-    // console.log(`Phone: ${Phone} Already in DB`);
+    if (e.name == "SequelizeUniqueConstraintError") {
+      console.log(`Name: ${Name} Already in DB`);
+    } else {
+      console.log(`Error`, e);
+    }
   }
 }
 //--------------------------------------------------------------------------
 export async function bulk_insert(interns) {
   let promise = [];
   const length = interns.length; // Assuming emails, names, and phone have the same length
-
-  console.log(length);
-
   for (let i = 0; i < length; i++) {
-    // console.log(`Email: ${interns[i].Email}`);
-    // console.log(`Name: ${interns[i].Name}`);
-    // console.log(`Phone: ${interns[i].Phone}`);
-
     let response = insert_interns(interns[i]);
     promise.push(response);
   }
