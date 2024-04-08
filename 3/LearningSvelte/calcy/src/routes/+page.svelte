cb6c5adee2b6bf8242e632949a068c13d881bc0e
DONTKILLME <ashishnegi0511@gmail.com>
2024-04-08 18:51:21 +0530

    Calculator completed

--- 3/LearningSvelte/calcy/src/routes/+page.svelte
+++ 3/LearningSvelte/calcy/src/routes/+page.svelte
@@ -1,21 +1,111 @@
 <script lang="ts">
 	const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'];
 	const operations = ['/', '*', '+', '-', '='];
+
+	// For storing which operations need to be performed /, *, +, -
+	let selectedOperation = '';
+	// For displaying required content on the main screen.
+	let display = '';
+	// Operations will be performed on two numbers and hence we need 2 variables to store them.
+	let firstNumber = '';
+	let secondNumber = '';
+	// Once the result is calculated we need to reset the firstNumber and SecondNumber to make the next operation possible without any bugs.
+	let isDisplayingResults = false;
+
+	const handleOperationClick = (operation: string) => {
+		// Scenario where the first number is not selected, we dont allow the user to select any operations.
+		if (!firstNumber) return;
+		// Scenario where the selected operation is '='
+		if (operation == '=') {
+			// Scenario where the second number is not selected, we dont allow the user to perform any operations.
+			if (!secondNumber) return;
+			const firstNum = parseInt(firstNumber);
+			const secondNum = parseInt(secondNumber);
+
+			let results = '';
+			// Switch cases for all scenarios
+			switch (selectedOperation) {
+				case '/':
+					results = (firstNum / secondNum).toFixed(2);
+					break;
+				case '*':
+					results = (firstNum * secondNum).toFixed(2);
+					break;
+				case '+':
+					results = (firstNum + secondNum).toFixed(2);
+					break;
+				case '-':
+					results = (firstNum - secondNum).toFixed(2);
+					break;
+			}
+			display = results;
+			isDisplayingResults = true;
+		}
+		selectedOperation = operation;
+	};
+
+	const handleClear = () => {
+		selectedOperation = '';
+		display = '';
+		firstNumber = '';
+		secondNumber = '';
+		isDisplayingResults = false;
+	};
+
+	const handleNumberClick = (number: string) => {
+		if (isDisplayingResults) {
+			handleClear();
+		}
+		// Scenario where the string is empty and the user is adding zeroes, resulting in the eventual magnitude of the numbers becoming zero.
+		if (display === '' && number === '0') return;
+
+		// Scenario where the string already has a decimal and user is trying to add more point's, eventually making a invalid number.
+		if (number === '.' && display.includes('.')) return;
+
+		if (!selectedOperation) {
+			// Scenario where the string is empty adn user is adding decimal in the begning.
+			if (display === '' && number === '.') {
+				firstNumber = '0.';
+				return (display = firstNumber);
+			}
+			firstNumber += number;
+			return (display = firstNumber);
+		} else {
+			// console.log('here', display);
+			// Scenario where the string is empty adn user is adding decimal in the begning.
+			if (display === '' && number === '.') {
+				secondNumber = '0.';
+				return (display = secondNumber);
+			}
+			secondNumber += number;
+			return (display = secondNumber);
+		}
+	};
 </script>
 
 <main>
 	<div class="calculator">
-		<div class="results"></div>
+		<div class="results">{display}</div>
 		<div class="digits">
 			<div class="numbers">
-				<button class="btn btn-xlg">Clear</button>
+				<button class="btn btn-xlg" on:click={handleClear}>Clear</button>
 				{#each numbers as number (number)}
-					<button class={`btn ${number === '0' ? 'btn-lg' : null}`}> {number} </button>
+					<button
+						class={`btn ${number === '0' ? 'btn-lg' : null}`}
+						on:click={() => handleNumberClick(number)}
+					>
+						{number}
+					</button>
 				{/each}
 			</div>
 			<div class="operations">
 				{#each operations as operation (operation)}
-					<button class="btn btn-orange"> {operation} </button>
+					<button
+						class={`btn ${operation === selectedOperation ? 'btn-silver' : 'btn-orange'}`}
+						on:click={() => handleOperationClick(operation)}
+					>
+						{operation}
+					</button>
 				{/each}
 			</div>
 		</div>
@@ -66,7 +156,7 @@
 		background-color: rgb(243, 100, 43);
 	}
 	.btn-silver {
-		background-color: silver;
+		background-color: rgb(216, 148, 148);
 	}
 	.btn-xlg {
 		width: 170px;
