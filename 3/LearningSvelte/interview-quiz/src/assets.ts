edc1ba4b22b5a1554b2a7fd8d02adf8da450e588
DONTKILLME <ashishnegi0511@gmail.com>
2024-04-10 16:13:27 +0530

    Article One finished

---
+++ 3/LearningSvelte/interview-quiz/src/assets.ts
@@ -0,0 +1,6 @@
+export const physics = 'https://image.lexica.art/full_webp/12aa90a3-5287-4729-9143-19c84a166a20';
+
+export const mathematics =
+	'https://image.lexica.art/full_webp/6bf9623e-37cd-4abf-9ea5-57bbb15f5df1';
+
+export const computer = 'https://image.lexica.art/full_webp/42a1376f-b990-44a1-bf81-dee814939cca';
--- 3/LearningSvelte/interview-quiz/src/routes/+page.svelte
+++ 3/LearningSvelte/interview-quiz/src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import * as images from '../assets';
+	import quizzes from '../questions.json';
+</script>
+
+<div class="quizzes-container w-[700px] m-auto flex justify-center h-screen items-center">
+	{#each quizzes as quiz (quiz.id)}
+		<button class="quiz-card shadow w-[250px] rounded overflow-hidden cursor-pointer m-2">
+			<img class="h-44" src={images[quiz.imageUrl]} alt="" />
+			<div class="flex items-center justify-between py-3 px-2">
+				<h1 class="font-bold">{quiz.name}</h1>
+				<p>{quiz.questions.length} questions</p>
+			</div>
+		</button>
+	{/each}
+</div>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
+</style>
