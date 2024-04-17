5a49a018982b4cc3b7eff9590791aeadc00c157e
DONTKILLME <ashishnegi0511@gmail.com>
2024-04-17 12:15:27 +0530

    Step one of final article completed.

--- 3/LearningSvelte/interview-quiz/src/routes/quiz/[id]/+page.svelte
+++ 3/LearningSvelte/interview-quiz/src/routes/quiz/[id]/+page.svelte
@@ -2,18 +2,64 @@
 	import QuestionText from './components/QuestionText.svelte';
 	import QuestionOption from './components/QuestionOption.svelte';
 	import QuestionButton from './components/QuestionButton.svelte';
+	//This
+	import { answers, type Answer } from '../../../store';
+
+	//This
+	import { goto } from '$app/navigation';
+
 	export let data: any;
 
 	let currentQuestionIndex = 0;
 	$: question = data.questions[currentQuestionIndex];
+
+	let answersValue: Answer[];
+	answers.subscribe((value) => {
+		answersValue = value;
+	});
+
+	let selectedOption: null | string = null;
+	const handleChangeOption = (label: string) => {
+		selectedOption = label;
+	};
+
+	//This
+	let showCorrectAnswer = false;
+	const handleSubmit = () => {
+		//Dont allow function to proceed if option is not selected.
+		if (!selectedOption) return;
+
+		showCorrectAnswer = true;
+		answers.update((currentState) => {
+			const updatedAnswerState = currentState;
+			updatedAnswerState[currentQuestionIndex].isCorrect = selectedOption === question.answer;
+			return updatedAnswerState;
+		});
+	};
+
+	const handleNext = () => {
+		showCorrectAnswer = false;
+		selectedOption = null;
+		if (currentQuestionIndex === data.questions.length - 1) {
+			goto('/results');
+		} else {
+			currentQuestionIndex++;
+		}
+	};
 </script>
 
 <div>
 	<QuestionText text={question.question} />
 	<div class="flex justify-between flex-wrap cursor-pointer">
 		{#each question.options as option (option.id)}
-			<QuestionOption {option} />
+			<QuestionOption
+				{option}
+				{selectedOption}
+				{handleChangeOption}
+				{showCorrectAnswer}
+				answer={question.answer}
+			/>
 		{/each}
 	</div>
-	<QuestionButton />
+	<QuestionButton {handleSubmit} {handleNext} {showCorrectAnswer} />
 </div>
---
+++ 3/LearningSvelte/interview-quiz/src/store.ts
@@ -0,0 +1,8 @@
+import { writable } from 'svelte/store';
+
+export interface Answer {
+	id: number;
+	isCorrect: boolean | null;
+}
+
+export const answers = writable<Answer[]>([]);
